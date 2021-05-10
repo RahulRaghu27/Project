47b1547beb46cf10d56eb02141e3d2034d9907e3
Rahul R <Rahul R@Rahul>
2021-05-10 17:48:41 +0530

    display2 file in samplex branch

---
+++ SimpleJava/src/com/local/simple/display2.java
@@ -0,0 +1,10 @@
+package com.local.simple;
+
+public class display2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi Rahul, Welcome");
+	}
+
+}
