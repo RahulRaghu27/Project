882d091888958bb85ed6597fe3f513c7d11aea88
Rahul R <Rahul R@Rahul>
2021-05-10 18:13:21 +0530

    sysout added

--- SimpleJava/src/com/local/simple/display2.java
+++ SimpleJava/src/com/local/simple/display2.java
@@ -5,6 +5,7 @@ public class display2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hi Rahul, Welcome");
+System.out.println("Ooh Oay");
 	}
 
 }
