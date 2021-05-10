bdb0a830bb0c21e8bb451c32a97b57882820456a
Rahul R <Rahul R@Rahul>
2021-05-10 17:10:25 +0530

    This is my first commit

---
+++ SimpleJava/src/com/local/simple/display.java
@@ -0,0 +1,11 @@
+package com.local.simple;
+
+public class display {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to world!!");
+	}
+
+}
